c6256d88961dafaca12c319ad8b552c5f92fb3eb
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-07-13 17:47:18 +0300

    log disk usage a few times during imamge building

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,8 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
 apt-get install -yqq python3-pip python3-venv git curl unzip \
     dnsutils iptables docker.io
+
+df -h 2>&1
 apt-get clean
 rm -rf /var/lib/apt/lists/*
+df -h 2>&1
+
 adduser vagrant docker
 
 pip3 install pipenv
@@ -13,6 +17,10 @@ pipenv --version
 
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/es.conf
 sysctl --system
+
+echo "Disk & mem stats"
+df -h 2>&1
+free -h 2>&1
 
 sleep 1; until docker version; do sleep 1; done
 echo "Docker installed, all good"
