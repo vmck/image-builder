93cbc458b46052763e476a6d9738452e059536ca
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-07-13 17:37:03 +0300

    Install everything like in cluster's Dockerfile

--- scripts/master.sh
+++ scripts/master.sh
@@ -2,10 +2,17 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
-apt-get install -yqq git python3 unzip docker.io supervisor python3-venv
+apt-get install -yqq python3-pip python3-venv git curl unzip \
+    dnsutils iptables docker.io
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+adduser vagrant docker
+
+pip3 install pipenv
+pipenv --version
+
 echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/es.conf
 sysctl --system
-adduser vagrant docker
 
 sleep 1; until docker version; do sleep 1; done
 echo "Docker installed, all good"
