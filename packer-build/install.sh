88b5263a166b6fbf85b625ba41221ffc34c8b605
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-03-18 22:30:32 +0200

    Rebase

--- packer-build/install.sh
+++ packer-build/install.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-apt-get install -yq build-essential unzip xterm tmux openssh-server
+apt-get install -yq build-essential unzip python3-pip docker.io git dnsutils iptables curl
+sudo -H pip3 install pipenv
+sudo adduser vagrant docker
