d4efd3bbdf6bec00fd41c495af8d2670c35b6ba8
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2021-01-06 23:46:52 +0200

    Update install.sh

--- packer-build/install.sh
+++ packer-build/install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-apt-get install -yq build-essential unzip xterm tmux openssh-server
 
 # Apt database update
 apt update
 
-# Install wget curl and coreutils
-apt install -y wget curl coreutils
+apt-get install -yq 
 
-apt install -y                       \
-    gcc-8                                   \
-    python3 python3-pip                     \
-    perl                                    \
-    make gdb valgrind \
+apt install -yq
+    build-essential unzip xterm tmux openssh-server \
+    wget curl coreutils                             \
+    gcc-8                                           \
+    python3 python3-pip                             \
+    perl                                            \
+    make gdb valgrind                               \
     imagemagick
 
 # all py packages json os pathlib recordclass subprocess sys
-sudo pip3 install                           \
+sudo pip3 install -yq                       \
     argparse                                \
     pathlib                                 \
-    recordclass \
+    recordclass                             \
     pillow
 
-wget https://static.dfilip.xyz/pc2020-tema3-checker.zip -O /home/vagrant/pc2020-tema3-checker.zip
+wget -q https://static.dfilip.xyz/pc2020-tema3-checker.zip -O /home/vagrant/pc2020-tema3-checker.zip
