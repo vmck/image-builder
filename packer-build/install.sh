2eb0f520d4211e1dc8749f904e4e586eec91f107
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2021-12-18 00:03:26 +0200

    Update install.sh

--- packer-build/install.sh
+++ packer-build/install.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 apt-get install -yq build-essential unzip xterm tmux openssh-server
+apt-get install -yq gcc g++ openmpi-bin openmpi-common openmpi-doc libopenmpi-dev bc
+echo "Build install.sh done"
