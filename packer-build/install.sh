aa9f3e21fcfd697ecedbd97a62b396b0c1965b13
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2022-01-12 05:26:22 +0200

    PCLP1-Tema3-2021

--- packer-build/install.sh
+++ packer-build/install.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
-apt-get install -yq build-essential unzip xterm tmux openssh-server
+
+# Apt database update
+apt update
+
+apt install -yq \
+    build-essential unzip xterm tmux openssh-server \
+    wget curl coreutils                             \
+    gcc                                             \
+    python3 python3-pip                             \
+    perl                                            \
+    make gdb valgrind                               \
+    imagemagick
+
+pip3 install -q                         \
+    argparse                                \
+    pathlib                                 \
+    recordclass                             \
+    pillow
+
+wget -q https://static.dfilip.xyz/pc2020-tema3-checker.zip -O /usr/lib/pc2021-tema3-checker.zip
