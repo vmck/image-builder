8587aaf5b0a574b279dff33eae247175f9632c85
Dorinel Filip <dorinel.filip@gmail.com>
2022-05-27 02:32:58 +0300

    Update install.sh

--- packer-build/install.sh
+++ packer-build/install.sh
@@ -6,16 +6,6 @@ apt update
 apt install -yq \
     build-essential unzip xterm tmux openssh-server \
     wget curl coreutils                             \
-    gcc                                             \
-    python3 python3-pip                             \
-    perl                                            \
-    make gdb valgrind                               \
-    imagemagick
+    gcc make
 
-pip3 install -q                         \
-    argparse                                \
-    pathlib                                 \
-    recordclass                             \
-    pillow
-
-wget -q https://static.dfilip.xyz/pc2020-tema3-checker.zip -O /usr/lib/pc2020-tema3-checker.zip
+echo "tom ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
